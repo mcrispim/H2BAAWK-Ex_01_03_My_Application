38754f0a05e54d8c933331166163956f8b1ce771
Mauro Crispim <mcrispim@hotmail.com>
2022-02-26 15:55:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/Ex_01_03_myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.Ex_01_03_myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")    }
+}
